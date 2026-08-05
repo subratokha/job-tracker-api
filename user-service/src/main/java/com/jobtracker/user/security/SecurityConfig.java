70a140fb78bc3480fe7187d737048d41c9f564bc
Subrato Kha <shubrato1@gmail.com>
2026-08-06 00:16:07 +0200

    Addes swager endpoint and depdnecies and whitelisting in security

--- user-service/src/main/java/com/jobtracker/user/security/SecurityConfig.java
+++ user-service/src/main/java/com/jobtracker/user/security/SecurityConfig.java
@@ -34,7 +34,9 @@ public class SecurityConfig {
                                         "/auth/register",
                                         "/v3/api-docs/**",
                                         "/swagger-ui/**",
-                                        "/swagger-ui.html"
+                                        "/swagger-ui.html",
+                                        "/actuator/health",
+                                        "/actuator/health/**"
                                 ).permitAll()
                                 .anyRequest()
                                 .authenticated()
